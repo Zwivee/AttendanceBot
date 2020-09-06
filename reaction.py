bf8e060dedbaebe816d875fd7cb472d707bd7b1d
Tony <antonio.martinez.93021@gmail.com>
2020-09-05 22:08:41 -0700

    Add initial state of script. Connects bot to discord and searches for reactions.

---
+++ reaction.py
@@ -0,0 +1,24 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Bot is logged in.")
+
+@client.event
+async def on_reaction_add(reaction, user):
+    channel = reaction.message.channel
+    await client.send_message(channel, '{} has added {} to the message: {}'.format(user.name,reaction.emoji,reaction.message.content))
+
+@client.event
+async def on_reaction_remove(reaction, user):
+    channel = reaction.message.channel
+    await client.send_message(channel, '{} has removed {} from the message: {}'.format(user.name,reaction.emoji,reaction.message.content))
+
+client.run(TOKEN)
