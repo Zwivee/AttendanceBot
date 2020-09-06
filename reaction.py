717b1d4ed24e6186def3798e38534202d5b9aed7
Tony <antonio.martinez.93021@gmail.com>
2020-09-06 12:22:38 -0700

    Feat: Add filter to output of datetime - Output timedate for ✅ - Output timedate for \U0001fa91

--- reaction.py
+++ reaction.py
@@ -1,24 +1,46 @@
+import datetime
 import discord
+# import json
 import os
+
+# from discord.ext import commands
 from dotenv import load_dotenv
+
+# #instantiate an empty dict
+# attendance = {}
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+# #client = commands.Bot(command_prefix='!')
+# with open("attendance.json") as f:
+#     attendance = json.load(f)
+# default_prefix = "!"
 
 @client.event
 async def on_ready():
     print("Bot is logged in.")
 
+
 @client.event
 async def on_reaction_add(reaction, user):
     channel = reaction.message.channel
-    await client.send_message(channel, '{} has added {} to the message: {}'.format(user.name,reaction.emoji,reaction.message.content))
+    currentDate = datetime.datetime.now()
+    await client.send_message(channel, '{} has added {} to the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
+    if reaction.emoji == '✅' or reaction.emoji == '\U0001fa91':
+        await client.send_message(channel, currentDate)
+        # attendance[user.name] = currentDate
+
 
 @client.event
 async def on_reaction_remove(reaction, user):
     channel = reaction.message.channel
-    await client.send_message(channel, '{} has removed {} from the message: {}'.format(user.name,reaction.emoji,reaction.message.content))
+    await client.send_message(channel, '{} has removed {} from the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
+
+# @client.command(name='nw', help='Generates list of people who reacted to message with ✅ or \U0001fa91')
+# async def nw(ctx):
+#     with open('mydata.json', 'w') as f:
+#         json.dump(attendance, f)
 
 client.run(TOKEN)
