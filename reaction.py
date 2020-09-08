176f497350bc9967fcfdf15961541b8a3a181321
Zwivee <40147473+Zwivee@users.noreply.github.com>
2020-09-08 15:47:39 -0700

    Add on react removal functionality - When reaction is removed it will remove the dict from the list of json - Add clear function on start up of bot, so json file starts empty every run.

--- reaction.py
+++ reaction.py
@@ -1,4 +1,3 @@
-import datetime
 import discord
 import json
 import os
@@ -13,23 +12,30 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 position = 1
+
+# Serialize data into json format
 def write_json(data, filename='attendance.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
 
+
 @client.event
 async def on_ready():
     print("Bot is logged in.")
+    with open('attendance.json') as json_file:
+        # Deserialize data from json to list
+        data = json.load(json_file)
+
+        temp = data['usr_details']
+        # Clear data list of all data
+        temp.clear()
+        write_json(data)
 
 
 @client.event
 async def on_reaction_add(reaction, user):
     global position
-    channel = reaction.message.channel
-    currentDate = datetime.datetime.now()
-    await client.send_message(channel, '{} has added {} to the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
     if reaction.emoji == '✅' or reaction.emoji == '\U0001fa91':
-        await client.send_message(channel, currentDate)
         with open('attendance.json') as json_file:
             data = json.load(json_file)
 
@@ -38,11 +44,11 @@ async def on_reaction_add(reaction, user):
             # python object to appended
             if reaction.emoji == '✅':
                 status = 'yes'
-            elif reaction.emoji =='\U0001fa91':
+            elif reaction.emoji == '\U0001fa91':
                 status = 'chair'
             y = {"name": user.name,
-                "status": status,
-                "position": position
+                 "status": status,
+                 "position": position
                  }
 
             # appending data to emp_details
@@ -51,15 +57,21 @@ async def on_reaction_add(reaction, user):
             write_json(data)
             position += 1
 
+
 @client.event
 async def on_reaction_remove(reaction, user):
-    channel = reaction.message.channel
-    await client.send_message(channel, '{} has removed {} from the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
+    global position
     if reaction.emoji == '✅' or reaction.emoji == '\U0001fa91':
-        with open('attendance.json') as json_file:
-            data = json.load(json_file)
-        for element in data:
-           del element["user.name"]
+        to_keep = []
 
-    write_json(data)
+        with open('attendance.json', 'r') as json_file:
+            data = json.load(json_file)
+            temp = data['usr_details']
+            for i in range(len(temp)):
+                if temp[i]['name'] == user.name:
+                    del temp[i]
+
+            write_json(data)
+            position -= 1
+
 client.run(TOKEN)
