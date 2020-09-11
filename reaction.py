e960220f845364e644ec16b90af8b91cf9dba454
Tony <antonio.martinez.93021@gmail.com>
2020-09-11 13:08:37 -0700

    Add: Restructure json file Simplified json to be two lists of names. 1. Chair list is the list of names that match people who reacted to the message with chair 2. Yes list is the list of names that match people who reacted to the message with yes

--- reaction.py
+++ reaction.py
@@ -5,73 +5,63 @@ import os
 from dotenv import load_dotenv
 from json import JSONEncoder
 
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
-position = 1
-
 # Serialize data into json format
 def write_json(data, filename='attendance.json'):
     with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
-
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 @client.event
 async def on_ready():
     print("Bot is logged in.")
-    with open('attendance.json') as json_file:
-        # Deserialize data from json to list
+    with open('attendance.json', 'r') as json_file:
+        # Deserialize data from json to lists
         data = json.load(json_file)
+        yesList = data['✅']
+        chairList= data['🪑']
 
-        temp = data['usr_details']
         # Clear data list of all data
-        temp.clear()
-        write_json(data)
+        yesList.clear()
+        chairList.clear()
 
+        write_json(data)
 
 @client.event
 async def on_reaction_add(reaction, user):
-    global position
-    if reaction.emoji == '✅' or reaction.emoji == '\U0001fa91':
-        with open('attendance.json') as json_file:
-            data = json.load(json_file)
+    newPerson=user.name
+    with open('attendance.json', 'r') as json_file:
+        data = json.load(json_file)
+        yesList=data['✅']
+        chairList=data['🪑']
 
-            temp = data['usr_details']
-
-            # python object to appended
-            if reaction.emoji == '✅':
-                status = 'yes'
-            elif reaction.emoji == '\U0001fa91':
-                status = 'chair'
-            y = {"name": user.name,
-                 "status": status,
-                 "position": position
-                 }
-
-            # appending data to emp_details
-            temp.append(y)
-
-            write_json(data)
-            position += 1
-
+        if reaction.emoji == '✅':
+            #Append to yesList
+            yesList.append(newPerson)
+        elif reaction.emoji == '🪑':
+            chairList.append(newPerson)
+        with open('attendance.json', 'w') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 @client.event
 async def on_reaction_remove(reaction, user):
-    global position
+
     if reaction.emoji == '✅' or reaction.emoji == '\U0001fa91':
         to_keep = []
 
         with open('attendance.json', 'r') as json_file:
             data = json.load(json_file)
-            temp = data['usr_details']
+            temp = data['attendance']
             for i in range(len(temp)):
                 if temp[i]['name'] == user.name:
-                    del temp[i]
+                    print(i)
+                    temp.pop(i)
+                    print("Got here")
 
-            write_json(data)
-            position -= 1
+            with open('attendance.json', 'w') as json_file:
+                json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 client.run(TOKEN)
