41f2243db6aefeac27cb629459eb0bc3a1e038d3
Tony <antonio.martinez.93021@gmail.com>
2020-09-11 15:07:16 -0700

    Doc: Added comments to code

--- reaction.py
+++ reaction.py
@@ -7,10 +7,12 @@ from dotenv import load_dotenv
 from discord.ext import commands
 from json import JSONEncoder
 
+# Load environment settings for discord token
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 prefix='!'
 
+# Initialization
 client = discord.Client()
 client = commands.Bot(command_prefix = prefix)
 client.remove_command('help')
@@ -20,6 +22,7 @@ def write_json(data, filename='attendance.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
+# Remove all data from json file
 def clear_json():
     with open('attendance.json', 'r') as json_file:
         # Deserialize data from json to lists
@@ -32,7 +35,7 @@ def clear_json():
         chairList.clear()
 
         write_json(data)
-
+# Bot started
 @client.event
 async def on_ready():
     print("Bot is logged in.")
@@ -40,32 +43,45 @@ async def on_ready():
 
 @client.event
 async def on_reaction_add(reaction, user):
+    # Name of user that added reacted to message
     newPerson=user.name
+    # Open json in read mode
     with open('attendance.json', 'r') as json_file:
+        # Deserialize json file
         data = json.load(json_file)
+        # Get pointer to lists
         yesList=data['✅']
         chairList=data['🪑']
 
     if reaction.emoji == '✅':
-        #Append to yesList
+        # Append to yesList
         yesList.append(newPerson)
     elif reaction.emoji == '🪑':
+        # Append to chairList
         chairList.append(newPerson)
 
     with open('attendance.json', 'w') as json_file:
+        # Serialize json data and format it. Allow for ascii character to show
         json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 @client.event
 async def on_reaction_remove(reaction, user):
+    # Name of user that removed reaction
     newPerson=user.name
+    # Open json in read mode
     with open('attendance.json', 'r') as json_file:
+        # Deserialize json file
         data = json.load(json_file)
+        # Get pointer to lists
         yesList=data['✅']
         chairList=data['🪑']
 
     if reaction.emoji == '✅':
+        # Search the length of list
         for i in range(len(yesList)):
+            # Name matching person that removed reaction found
             if yesList[i] == user.name:
+                # Remove user
                 yesList.pop(i)
     elif reaction.emoji == '🪑':
         for i in range(len(chairList)):
@@ -83,5 +99,5 @@ async def attendance():
 
     # Clear the json again to start new tally
     clear_json()
-
+# Run bot
 client.run(TOKEN)
