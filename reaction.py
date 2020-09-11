a46c8318b6dfa9ed51f13446bac83985916d2365
Tony <antonio.martinez.93021@gmail.com>
2020-09-11 14:58:02 -0700

    Feat: Add export of json to excel

--- reaction.py
+++ reaction.py
@@ -1,23 +1,26 @@
 import discord
 import json
 import os
+import pandas as pd
 
 from dotenv import load_dotenv
+from discord.ext import commands
 from json import JSONEncoder
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+prefix='!'
 
 client = discord.Client()
+client = commands.Bot(command_prefix = prefix)
+client.remove_command('help')
 
 # Serialize data into json format
 def write_json(data, filename='attendance.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-@client.event
-async def on_ready():
-    print("Bot is logged in.")
+def clear_json():
     with open('attendance.json', 'r') as json_file:
         # Deserialize data from json to lists
         data = json.load(json_file)
@@ -31,6 +34,11 @@ async def on_ready():
         write_json(data)
 
 @client.event
+async def on_ready():
+    print("Bot is logged in.")
+    clear_json()
+
+@client.event
 async def on_reaction_add(reaction, user):
     newPerson=user.name
     with open('attendance.json', 'r') as json_file:
@@ -38,30 +46,42 @@ async def on_reaction_add(reaction, user):
         yesList=data['✅']
         chairList=data['🪑']
 
-        if reaction.emoji == '✅':
-            #Append to yesList
-            yesList.append(newPerson)
-        elif reaction.emoji == '🪑':
-            chairList.append(newPerson)
-        with open('attendance.json', 'w') as json_file:
-            json.dump(data, json_file, ensure_ascii=False, indent=4)
+    if reaction.emoji == '✅':
+        #Append to yesList
+        yesList.append(newPerson)
+    elif reaction.emoji == '🪑':
+        chairList.append(newPerson)
+
+    with open('attendance.json', 'w') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 @client.event
 async def on_reaction_remove(reaction, user):
+    newPerson=user.name
+    with open('attendance.json', 'r') as json_file:
+        data = json.load(json_file)
+        yesList=data['✅']
+        chairList=data['🪑']
 
-    if reaction.emoji == '✅' or reaction.emoji == '\U0001fa91':
-        to_keep = []
+    if reaction.emoji == '✅':
+        for i in range(len(yesList)):
+            if yesList[i] == user.name:
+                yesList.pop(i)
+    elif reaction.emoji == '🪑':
+        for i in range(len(chairList)):
+            if chairList[i] == user.name:
+                chairList.pop(i)
 
-        with open('attendance.json', 'r') as json_file:
-            data = json.load(json_file)
-            temp = data['attendance']
-            for i in range(len(temp)):
-                if temp[i]['name'] == user.name:
-                    print(i)
-                    temp.pop(i)
-                    print("Got here")
-
-            with open('attendance.json', 'w') as json_file:
+    with open('attendance.json', 'w') as json_file:
                 json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+@client.command()
+async def attendance():
+    # Pandas to translate from json to xls
+    df_json = pd.read_json('attendance.json')
+    df_json.to_excel('attendance.xls')
+
+    # Clear the json again to start new tally
+    clear_json()
 
 client.run(TOKEN)
