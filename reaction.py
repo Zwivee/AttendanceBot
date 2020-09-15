24656be1fa6bd7da17b0918dd2cce6612c734fae
Antonio M <40147473+Zwivee@users.noreply.github.com>
2020-09-14 22:53:49 -0700

    Develop (#4)
    * Add initial state of script. Connects bot to discord and searches for reactions.
    
    * Feat: Add filter to output of datetime
    - Output timedate for ✅
    - Output timedate for \U0001fa91
    
    * Add: Add json appending
    
    * Restructured the json file and updated removal of commented out code.
    
    * Add on react removal functionality
    - When reaction is removed it will remove the dict from the list of json
    - Add clear function on start up of bot, so json file starts empty every run.
    
    * Add: Restructure json file
    Simplified json to be two lists of names.
    1. Chair list is the list of names that match people who reacted to the message with chair
    2. Yes list is the list of names that match people who reacted to the message with yes
    
    * Feat: Add export of json to excel
    
    * Doc: Added comments to code
    
    * Fix: Change json file to simple lists
    - Made the data structure to save the names of members into as lists.
     1. This allows pandas to run to_excel()
    
    * Feat: Add reply message on command.
    
    * Docs: Add finish readme

---
+++ reaction.py
@@ -0,0 +1,84 @@
+import discord
+import os
+import pandas as pd
+
+from dotenv import load_dotenv
+from discord.ext import commands
+from datetime import date
+
+# Load environment settings for discord token
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+prefix='!'
+
+# Initialization discord
+bot = commands.Bot(command_prefix = prefix)
+bot.remove_command('help')
+
+# Initialize empty lists
+yesList = []
+chairList= []
+
+# bot started
+@bot.event
+async def on_ready():
+    print("Bot is logged in.")
+    yesList.clear()
+    chairList.clear ()
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    # Name of user that added reacted to message
+    newPerson=user.name
+
+    if reaction.emoji == '✅':
+        # Append to yesList
+        yesList.append(newPerson)
+    elif reaction.emoji == '🪑':
+        # Append to chairList
+        chairList.append(newPerson)
+
+@bot.event
+async def on_reaction_remove(reaction, user):
+    # Name of user that removed reaction
+    newPerson=user.name
+
+    if reaction.emoji == '✅':
+        # Search the length of list
+        for i in range(len(yesList)):
+            # Name matching person that removed reaction found
+            if yesList[i] == user.name:
+                # Remove user
+                yesList.pop(i)
+    elif reaction.emoji == '🪑':
+        for i in range(len(chairList)):
+            if chairList[i] == user.name:
+                chairList.pop(i)
+
+@bot.command(pass_context=True)
+async def attendance(ctx):
+
+    df = pd.DataFrame()
+
+    # Create two columns
+    # Workaround for imbalanced lists in Pandas. Make lists into series first
+    # before export to fill all empty elements in list with NaN.
+    df['✅'] = pd.Series(yesList)
+    df['🪑'] = pd.Series(chairList)
+
+    # Convert to excel
+    df.to_excel('attendance.xls', index = False)
+
+    # Get current date
+    today = date.today()
+    d1 = today.strftime("%d/%m/%Y")
+
+    # Reply with message and current date.
+    await bot.send_message(ctx.message.channel,'Attendance excel generated on {}'.format(d1))
+
+    # Clear lists
+    yesList.clear()
+    chairList.clear()
+
+# Run bot
+bot.run(TOKEN)
