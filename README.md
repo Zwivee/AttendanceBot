0ef9fb2a40ca5399a3a7d1287626b886a9fd5fb8
Tony <antonio.martinez.93021@gmail.com>
2020-09-14 22:14:35 -0700

    Fix: Change json file to simple lists - Made the data structure to save the names of members into as lists.  1. This allows pandas to run to_excel()

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # AttendanceBot
-Discord bot that will track reactions on messages and export user information to determine order of reactions
+> Discord bot that tracks reactions to message, and provides command to export json data to excel spreadsheet.
+
+## Table of contents
+* [General info](#general-info)
+* []
+
+## General Info
+The need arose where tracking attendance for node wars in BDO for certain node
+sizes. The bot will track what the reaction of ✅ and 🪑.
+
+Where ✅ means the person will show up for node wars, and 🪑 meaning they can
+show but are okay with being 'benched'.
+
+## Setup
+
+Launch the bot,
--- reaction.py
+++ reaction.py
@@ -1,57 +1,44 @@
 import discord
-import json
 import os
 import pandas as pd
 
 from dotenv import load_dotenv
 from discord.ext import commands
-from json import JSONEncoder
+from datetime import date
+
+# TODO: Get rid of json file. Create two lists and append to them on reaction,
+# and remove pop from list when reaction removed. To export to excel use this
+# link
+# https://www.geeksforgeeks.org/python-split-given-list-and-insert-in-excel-file/
+# df['chairicon'] = list 1
+# df[''yesicon']= list 2
+# df.to_excel('result.xls', index=False)
 
 # Load environment settings for discord token
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 prefix='!'
 
-# Initialization
-client = discord.Client()
-client = commands.Bot(command_prefix = prefix)
-client.remove_command('help')
+# Initialization discord
+bot = discord.Client()
+bot = commands.Bot(command_prefix = prefix)
+bot.remove_command('help')
 
-# Serialize data into json format
-def write_json(data, filename='attendance.json'):
-    with open(filename, 'w') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+# Initialize empty lists
+yesList = []
+chairList= []
 
-# Remove all data from json file
-def clear_json():
-    with open('attendance.json', 'r') as json_file:
-        # Deserialize data from json to lists
-        data = json.load(json_file)
-        yesList = data['✅']
-        chairList= data['🪑']
-
-        # Clear data list of all data
-        yesList.clear()
-        chairList.clear()
-
-        write_json(data)
-# Bot started
-@client.event
+# bot started
+@bot.event
 async def on_ready():
     print("Bot is logged in.")
-    clear_json()
+    yesList.clear()
+    chairList.clear ()
 
-@client.event
+@bot.event
 async def on_reaction_add(reaction, user):
     # Name of user that added reacted to message
     newPerson=user.name
-    # Open json in read mode
-    with open('attendance.json', 'r') as json_file:
-        # Deserialize json file
-        data = json.load(json_file)
-        # Get pointer to lists
-        yesList=data['✅']
-        chairList=data['🪑']
 
     if reaction.emoji == '✅':
         # Append to yesList
@@ -60,21 +47,10 @@ async def on_reaction_add(reaction, user):
         # Append to chairList
         chairList.append(newPerson)
 
-    with open('attendance.json', 'w') as json_file:
-        # Serialize json data and format it. Allow for ascii character to show
-        json.dump(data, json_file, ensure_ascii=False, indent=4)
-
-@client.event
+@bot.event
 async def on_reaction_remove(reaction, user):
     # Name of user that removed reaction
     newPerson=user.name
-    # Open json in read mode
-    with open('attendance.json', 'r') as json_file:
-        # Deserialize json file
-        data = json.load(json_file)
-        # Get pointer to lists
-        yesList=data['✅']
-        chairList=data['🪑']
 
     if reaction.emoji == '✅':
         # Search the length of list
@@ -88,16 +64,27 @@ async def on_reaction_remove(reaction, user):
             if chairList[i] == user.name:
                 chairList.pop(i)
 
-    with open('attendance.json', 'w') as json_file:
-                json.dump(data, json_file, ensure_ascii=False, indent=4)
+@bot.command(pass_context=True)
+async def attendance(ctx):
 
-@client.command()
-async def attendance():
-    # Pandas to translate from json to xls
-    df_json = pd.read_json('attendance.json')
-    df_json.to_excel('attendance.xls')
+    df = pd.DataFrame()
 
-    # Clear the json again to start new tally
-    clear_json()
+    # Create two columns
+    # Workaround for imbalanced lists in Pandas. Make lists into series first
+    # before export to fill all empty elements in list with NaN.
+    df['✅'] = pd.Series(yesList)
+    df['🪑'] = pd.Series(chairList)
+
+    # Convert to excel
+    df.to_excel('attendance.xls', index = False)
+
+    # today = date.today()
+    # d1 = today.strftime("%d/%m/%Y")
+    # await bot.send_message(ctx.channel,'Attendance excel generated')
+
+    # Clear lists
+    yesList.clear()
+    chairList.clear()
+
 # Run bot
-client.run(TOKEN)
+bot.run(TOKEN)
