be18b234bb3663d97b064b7f844d2f5cb7dafd59
Zwivee <40147473+Zwivee@users.noreply.github.com>
2021-04-08 10:04:54 -0700

    add cap size change and null date check

--- reaction.py
+++ reaction.py
@@ -24,63 +24,83 @@ bot.remove_command('help')
 
 # Initialize gspread
 gc = gspread.service_account(filename='keys.json')
-
-print(SHEET)
 sh = gc.open_by_key(SHEET)
 worksheet = sh.worksheet('Master List')
 
+# Global settings
+# Currently if we want to restrain the number of people that can be checked, but this will become obsolete when uncapped begins
+global nodeCapacity
+nodeCapacity = 100
+
 # Bot started
+
+
 @bot.event
 async def on_ready():
     print("Bot is logged in.")
 
+def getUser(user):
+    # Name of user that added reacted to message
+    newPerson = user.display_name
+    inGameNameQ = newPerson.split(' ', 1)[0]
+    inGameName = inGameNameQ.replace('"', '')
+    return inGameName
+
+
+def calculateWeekdayFromAnnouncement(reaction):
+    # Find message date and give weekday in for of 0 Monday - 6 Sunday
+    message = reaction.message.content
+
+    match = re.search('\d{1}/\d{2}/\d{2}', message)
+    if match is not None:
+        nwWeekDay = datetime.datetime.strptime(
+            match.group(), '%m/%d/%y').date().weekday()
+        return nwWeekDay
+
+
+def updateCell(inGameName, nwWeekDay, status):
+    cell = worksheet.find(inGameName)
+    worksheet.update_cell(cell.row, nwWeekDay+11, status)
+
+
+def checkTodaysAttendanceInSheets(nwWeekDay):
+    currentAttendance = worksheet.cell(2,nwWeekDay+11).value
+    return int(currentAttendance)
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    if reaction.emoji == '✅':
-        # Name of user that added reacted to message
-        newPerson = user.display_name
-        inGameNameQ = newPerson.split(' ', 1)[0]
-        inGameName = inGameNameQ.replace('"', '')
-        message = reaction.message.content
-
-        # Find message date and give weekday in for of 0 Monday - 6 Sunday
-        match = re.search('\d{1}/\d{2}/\d{2}', message)
-        nwWeekDay = datetime.datetime.strptime(
-            match.group(), '%m/%d/%y').date().weekday()
-
-        cell = worksheet.find(inGameName)
-        worksheet.update_cell(cell.row,nwWeekDay+11,'TRUE')
+    inGameName = getUser(user)
+    nwWeekDay = calculateWeekdayFromAnnouncement(reaction)
+    if nwWeekDay is not None:
+        if (reaction.emoji == '✅') and (checkTodaysAttendanceInSheets(nwWeekDay) < int(nodeCapacity)):
+            updateCell(inGameName, nwWeekDay, 'TRUE')
 
 
 
 @bot.event
 async def on_reaction_remove(reaction, user):
     if reaction.emoji == '✅':
-        # Name of user that added reacted to message
-        newPerson = user.display_name
-        inGameNameQ = newPerson.split(' ', 1)[0]
-        inGameName = inGameNameQ.replace('"', '')
-        message = reaction.message.content
+        inGameName = getUser(user)
+        nwWeekDay = calculateWeekdayFromAnnouncement(reaction)
+        if nwWeekDay is not None:
+            updateCell(inGameName, nwWeekDay, 'FALSE')
 
-        # Find message date and give weekday in for of 0 Monday - 6 Sunday
-        match = re.search('\d{1}/\d{2}/\d{2}', message)
-        nwWeekDay = datetime.datetime.strptime(
-            match.group(), '%m/%d/%y').date().weekday()
-
-        cell = worksheet.find(inGameName)
-        worksheet.update_cell(cell.row,nwWeekDay+11,'FALSE')
 
 
 @bot.command(pass_context=True)
-async def attendance(ctx):
+async def kill(ctx):
+    # Allow any server administrators to kill the bottom
+    if ctx.message.author.server_permissions.administrator:
+        await bot.logout()
 
-    # Get current date
-    today = date.today()
-    d1 = today.strftime("%d/%m/%Y")
 
-    # Reply with message and current date.
-    await ctx.send('Attendance excel generated on {}'.format(d1))
+@bot.command(pass_context=True)
+async def cap(ctx, capacitySetting):
+    global nodeCapacity
+    nodeCapacity = capacitySetting
+
+    # Reply with message and new capacity set
+    await ctx.channel.send('Node Cap Set as {}'.format(nodeCapacity))
 
 # Run bot
 bot.run(TOKEN)
