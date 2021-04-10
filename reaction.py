5aa178787e763089c39ada0f64cd5a52d8044553
Zwivee <40147473+Zwivee@users.noreply.github.com>
2021-04-10 14:24:20 -0700

    Added function to reset cap at 18:00 & fixed issue with sunday attendance

--- reaction.py
+++ reaction.py
@@ -3,9 +3,10 @@ import os
 import re
 import datetime
 import gspread
+import asyncio
 
 from dotenv import load_dotenv
-from discord.ext import commands
+from discord.ext import commands, tasks
 from datetime import date
 
 # Load environment settings for discord token
@@ -38,6 +39,7 @@ nodeCapacity = 100
 async def on_ready():
     print("Bot is logged in.")
 
+
 def getUser(user):
     # Name of user that added reacted to message
     newPerson = user.display_name
@@ -52,8 +54,8 @@ def calculateWeekdayFromAnnouncement(reaction):
 
     match = re.search('\d{1}/\d{2}/\d{2}', message)
     if match is not None:
-        nwWeekDay = datetime.datetime.strptime(
-            match.group(), '%m/%d/%y').date().weekday()
+        nwWeekDay = datetime.datetime.strptime(match.group(),
+                                               '%m/%d/%y').date().weekday()
         return nwWeekDay
 
 
@@ -63,21 +65,29 @@ def updateCell(inGameName, nwWeekDay, status):
     except:
         print("Cannot find name")
     else:
-        worksheet.update_cell(cell.row, nwWeekDay+11, status)
+        # Since we do not node war on Saturday the sheet is missing a column and therefor sunday must be calculated with an offset
+        if nwWeekDay != 6:
+            worksheet.update_cell(cell.row, nwWeekDay + 11, status)
+        else:
+            worksheet.update_cell(cell.row, nwWeekDay + 10, status)
 
 
 def checkTodaysAttendanceInSheets(nwWeekDay):
-    currentAttendance = worksheet.cell(2,nwWeekDay+11).value
+    if nwWeekDay != 6:
+        currentAttendance = worksheet.cell(2, nwWeekDay + 11).value
+    else:
+        currentAttendance = worksheet.cell(2, nwWeekDay + 10).value
     return int(currentAttendance)
+
 
 @bot.event
 async def on_reaction_add(reaction, user):
     inGameName = getUser(user)
     nwWeekDay = calculateWeekdayFromAnnouncement(reaction)
     if nwWeekDay is not None:
-        if (reaction.emoji == '✅') and (checkTodaysAttendanceInSheets(nwWeekDay) < int(nodeCapacity)):
+        if (reaction.emoji == '✅') and (
+                checkTodaysAttendanceInSheets(nwWeekDay) < int(nodeCapacity)):
             updateCell(inGameName, nwWeekDay, 'TRUE')
-
 
 
 @bot.event
@@ -89,22 +99,46 @@ async def on_reaction_remove(reaction, user):
             updateCell(inGameName, nwWeekDay, 'FALSE')
 
 
-
-@bot.command(pass_context=True)
+@bot.command(pass_context=True, alias=["quit"])
+@commands.has_permissions(administrator=True)
 async def kill(ctx):
     # Allow any server administrators to kill the bottom
-    if ctx.message.author.server_permissions.administrator:
-        await bot.logout()
+    await bot.close()
+    print('Bot is logged out')
 
 
 @bot.command(pass_context=True)
+@commands.has_permissions(administrator=True)
 async def cap(ctx, capacitySetting):
-    if ctx.message.author.server_permissions.administrator:
-        global nodeCapacity
-        nodeCapacity = capacitySetting
+    global nodeCapacity
+    nodeCapacity = capacitySetting
 
-        # Reply with message and new capacity set
-        await ctx.channel.send('Node Cap: {}'.format(nodeCapacity))
+    # Reply with message and new capacity set
+    await ctx.channel.send('Node Cap: {}'.format(nodeCapacity))
+
+
+def seconds_until(hours, minutes):
+    given_time = datetime.time(hours, minutes)
+    now = datetime.datetime.now()
+    future_exec = datetime.datetime.combine(now, given_time)
+    if (
+            future_exec - now
+    ).days < 0:  # If we are past the execution, it will take place tomorrow
+        future_exec = datetime.datetime.combine(
+            now + datetime.timedelta(days=1), given_time)  # days always >= 0
+
+    return (future_exec - now).total_seconds()
+
+
+@tasks.loop(hours=24)
+async def resetCap():
+    await asyncio.sleep(seconds_until(18, 00))
+    global nodeCapacity
+    nodeCapacity = 100
+    print('Node Capacity Reset')
+
+
+resetCap.start()
 
 # Run bot
 bot.run(TOKEN)
