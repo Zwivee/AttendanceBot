3d2f7e17747a993424af276ea6371bb52949dab0
Zwivee <40147473+Zwivee@users.noreply.github.com>
2021-04-22 16:43:03 -0700

    Formatting: Adhere to PEP8 guidelines

--- reaction.py
+++ reaction.py
@@ -1,25 +1,24 @@
-import discord
 import os
 import re
 import datetime
-import gspread
 import asyncio
+import discord
+import gspread
 
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
-from datetime import date
 
 # Load environment settings for discord token
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SHEET = os.getenv('SHEET_ID')
-prefix = '!'
+PREFIX = '!'
 
 # Initialization discord
 intents = discord.Intents.default()
 intents.reactions = True
 intents.members = True
-bot = commands.Bot(command_prefix=prefix, intents=intents)
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 bot.remove_command('help')
 
 # Initialize gspread
@@ -28,17 +27,18 @@ sh = gc.open_by_key(SHEET)
 worksheet = sh.worksheet('Master List')
 
 # Global settings
-# Currently if we want to restrain the number of people that can be checked, but this will become obsolete when uncapped begins
-maxAttendace = 100
-global nodeCapacity
-nodeCapacity = maxAttendace
-colContainingNames = 2
-monToFriOffset = 11
-sundayOffset = 10
-sunday = 6
-nodeCapacityResetTimer = 24  # in hours
-hourOfDayToeset = 18  # military time
-minuteOfHourToReset = 00
+# Currently if we want to restrain the number of people
+# that can be checked, but this will become obsolete when uncapped begins
+MAX_ATTENDANCE = 100
+global NODE_CAPACITY
+NODE_CAPACITY = MAX_ATTENDANCE
+COL_CONTAIN_IN_GAMES = 2
+MON_TO_FRI_OFFSET = 11
+SUNDAY_OFFSET = 10
+SUNDAY = 6
+NODE_CAPACITY_RESET_TIMER = 24  # in hours
+HOUR_OF_DAY_TO_RESET = 18  # military time
+MINUTE_OF_HOUR_TO_RESET = 00
 
 
 # Bot started
@@ -48,70 +48,84 @@ async def on_ready():
 
 
 # Name of user that added reacted to message
-def getUser(user):
-    newPerson = user.display_name
-    inGameNameQ = newPerson.split(' ', 1)[0]
-    inGameName = inGameNameQ.replace('"', '')
-    return inGameName
+def get_user(user):
+    new_person = user.display_name
+    in_game_name_q = new_person.split(' ', 1)[0]
+    in_game_name = in_game_name_q.replace('"', '')
+    return in_game_name
 
 
 # Find message date and give weekday in for of 0 Monday - 6 Sunday
-def calculateWeekdayFromAnnouncement(reaction):
+def calculate_weekday_from_announcement(reaction):
     message = reaction.message.content
-
-    match = re.search('\d{1}/\d{2}/\d{2}', message)
+    match = re.search(r'\d{1}/\d{2}/\d{2}', message)
     if match is not None:
-        nwWeekDay = datetime.datetime.strptime(match.group(),
-                                               '%m/%d/%y').date().weekday()
-        return nwWeekDay
+        nw_weekday = datetime.datetime.strptime(match.group(),
+                                                '%m/%d/%y').date().weekday()
+        return nw_weekday
 
 
-# Find the cell in google sheet that matches inGameName and try to update the correct day
-def updateCell(inGameName, nwWeekDay, status):
+# Find the cell in google sheet that matches
+# inGameName and try to update the correct day
+def update_cell(in_game_name_update, target_weekday, status):
     try:
         # \b binds results to whole words, and used built in ignore case method
-        caseInsensitiveCheck = re.compile(rf"\b{inGameName}\b", re.IGNORECASE)
-        cell = worksheet.find(caseInsensitiveCheck)
-    except:
-        print("Cannot find name" + inGameName)
+        case_insensitive_check = re.compile(rf"\b{in_game_name_update}\b",
+                                            re.IGNORECASE)
+        cell = worksheet.find(case_insensitive_check)
+    except gspread.CellNotFound:
+        print("Cannot find name " + in_game_name_update)
     else:
-        # Since we do not node war on Saturday the sheet is missing a column and therefor sunday must be calculated with an offset
-        if nwWeekDay != sunday:
-            worksheet.update_cell(cell.row, nwWeekDay + monToFriOffset, status)
+        # Since we do not node war on Saturday the sheet is missing a column
+        # and therefore sunday must be calculated with an offset
+        if target_weekday != SUNDAY:
+            worksheet.update_cell(cell.row, target_weekday + MON_TO_FRI_OFFSET,
+                                  status)
         else:
-            worksheet.update_cell(cell.row, nwWeekDay + sundayOffset, status)
+            worksheet.update_cell(cell.row, target_weekday + SUNDAY_OFFSET,
+                                  status)
 
 
-# Since we do not node war on Saturday the sheet is missing a column and therefore sunday must be calculated with an offset.
-def checkTodaysAttendanceInSheets(nwWeekDay):
-    if nwWeekDay != sunday:
-        currentAttendance = worksheet.cell(colContainingNames,
-                                           nwWeekDay + monToFriOffset).value
+# Since we do not node war on Saturday the sheet is missing a column
+# and therefore sunday must be calculated with an offset.
+def check_todays_attendance_in_sheets(target_nw_weekday_to_check):
+    if target_nw_weekday_to_check != SUNDAY:
+        current_attendance = worksheet.cell(
+            COL_CONTAIN_IN_GAMES,
+            target_nw_weekday_to_check + MON_TO_FRI_OFFSET).value
     else:
-        currentAttendance = worksheet.cell(colContainingNames,
-                                           nwWeekDay + sundayOffset).value
-    return int(currentAttendance)
+        current_attendance = worksheet.cell(
+            COL_CONTAIN_IN_GAMES,
+            target_nw_weekday_to_check + SUNDAY_OFFSET).value
+    return int(current_attendance)
 
 
-# Triggers when message in channel has ✅ added from message. Reads the date and updates the correct cell in google sheets.
+# Triggers when message in channel has ✅ added from message
+# Reads the date and updates the correct cell in google sheets.
 @bot.event
 async def on_reaction_add(reaction, user):
-    inGameName = getUser(user)
-    nwWeekDay = calculateWeekdayFromAnnouncement(reaction)
-    if nwWeekDay is not None:
+    user_in_game_name = get_user(user)
+    current_nw_weekday = calculate_weekday_from_announcement(reaction)
+    # Check if officer posting did not place date in announcement
+    if current_nw_weekday is not None:
         if (reaction.emoji == '✅') and (
-                checkTodaysAttendanceInSheets(nwWeekDay) < int(nodeCapacity)):
-            updateCell(inGameName, nwWeekDay, 'TRUE')
+                check_todays_attendance_in_sheets(current_nw_weekday) <
+                int(NODE_CAPACITY)):
+            update_cell(user_in_game_name, current_nw_weekday, 'TRUE')
 
 
-# Triggers when message in channel has ✅ removed from message. Reads the date and updates the correct cell in google sheets.
+# Triggers when message in channel has ✅ removed from message
+# Reads the date and updates the correct cell in google sheets.
 @bot.event
 async def on_reaction_remove(reaction, user):
     if reaction.emoji == '✅':
-        inGameName = getUser(user)
-        nwWeekDay = calculateWeekdayFromAnnouncement(reaction)
-        if nwWeekDay is not None:
-            updateCell(inGameName, nwWeekDay, 'FALSE')
+        user_in_game_name = get_user(user)
+        current_nw_weekday = calculate_weekday_from_announcement(reaction)
+        # Check if officer posting did not place date in announcement
+        # TODO: Move this out to a Function
+        # TODO: Add Try-Catch for logging purposes
+        if current_nw_weekday is not None:
+            update_cell(user_in_game_name, current_nw_weekday, 'FALSE')
 
 
 # Handler for command errors
@@ -134,7 +148,7 @@ async def on_command_error(ctx, error):
 # Command !kill that can only be used by server administrators to stop the bot
 @bot.command(pass_context=True, alias=["quit"])
 @commands.has_permissions(administrator=True)
-async def Kill(ctx):
+async def kill():
     # Allow any server administrators to kill the bottom
     await bot.close()
     print('Bot is logged out')
@@ -143,50 +157,50 @@ async def Kill(ctx):
 # Command !cap that can only be used by server administrators to change the node war cap
 @bot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
-async def Cap(ctx, capacitySetting):
-    global nodeCapacity
-    nodeCapacity = capacitySetting
+async def cap(ctx, capacity_setting):
+    global NODE_CAPACITY
+    NODE_CAPACITY = capacity_setting
 
     # Reply with message and new capacity set
-    await ctx.channel.send('Node Cap: {}'.format(nodeCapacity))
+    await ctx.channel.send('Node Cap: {}'.format(NODE_CAPACITY))
 
 
 # Starts a command group for the help command
 # Command groups will expand on a certain command such as help <command>
 @bot.group(invoke_without_command=True)
 async def help(ctx):
-    em = discord.Embed(
+    embed_result = discord.Embed(
         title="Help",
         description=
         "Use !help <command> for extended information on a command.",
         color=ctx.author.color)
-    em.add_field(name="Moderation", value="Kill")
-    em.add_field(name="Basic", value="Cap")
+    embed_result.add_field(name="Moderation", value="Kill")
+    embed_result.add_field(name="Basic", value="Cap")
 
-    await ctx.send(embed=em)
-
-
-# Part of help command group expand on the help command
-# Will not return base help command, but will instead return declared embed
-@help.command()
-async def kill(ctx):
-    em = discord.Embed(title="Kill",
-                       description="Kill the bot",
-                       color=ctx.author.color)
-
-    await ctx.send(embed=em)
+    await ctx.send(embed=embed_result)
 
 
 # Part of help command group expand on the help command
 # Will not return base help command, but will instead return declared embed
 @help.command()
-async def cap(ctx):
-    em = discord.Embed(title="Cap",
-                       description="Changes attendace capacity.",
-                       color=ctx.author.color)
-    em.add_field(name="**Syntax**", value="!cap <number>")
+async def kill_help(ctx):
+    embed_result = discord.Embed(title="kill",
+                                 description="Kill the bot",
+                                 color=ctx.author.color)
 
-    await ctx.send(embed=em)
+    await ctx.send(embed=embed_result)
+
+
+# Part of help command group expand on the help command
+# Will not return base help command, but will instead return declared embed
+@help.command()
+async def cap_help(ctx):
+    embed_result = discord.Embed(title="cap",
+                                 description="Changes attendace capacity.",
+                                 color=ctx.author.color)
+    embed_result.add_field(name="**Syntax**", value="!cap <number>")
+
+    await ctx.send(embed=embed_result)
 
 
 # Timer to calculate when to run the task to reset nodewar cap automatically
@@ -204,15 +218,16 @@ def seconds_until(hours, minutes):
 
 
 # Resets the node war cap variable every day, current setting at 6pm pst v2.1.2
-@tasks.loop(hours=nodeCapacityResetTimer)
-async def resetCap():
-    await asyncio.sleep(seconds_until(hourOfDayToeset, minuteOfHourToReset))
-    global nodeCapacity
-    nodeCapacity = maxAttendace
+@tasks.loop(hours=NODE_CAPACITY_RESET_TIMER)
+async def reset_capacity():
+    await asyncio.sleep(
+        seconds_until(HOUR_OF_DAY_TO_RESET, MINUTE_OF_HOUR_TO_RESET))
+    global NODE_CAPACITY
+    NODE_CAPACITY = MAX_ATTENDANCE
     print('Node Capacity Reset')
 
 
-resetCap.start()
+reset_capacity.start()
 
 # Run bot
 bot.run(TOKEN)
